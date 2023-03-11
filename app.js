83a6b5720c5bbd2c19dd4955ce70dcf32c1e7d62
Binay Shaw <binayshaw7777@gmail.com>
2023-03-11 13:41:59 +0530

    Connected DB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ app.get('/', (req, res) => {
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGODB_URL);
         app.listen(PORT, () => {
             console.log(`Server is listening on port ${PORT}`);
         })
